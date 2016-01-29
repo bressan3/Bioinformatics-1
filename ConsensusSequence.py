1a0cbbe4ee814545767e7092072a848186dbb5e2
Lucas B <bressan3@gmail.com>
2016-01-28 22:46:17 -0500

    first commit

---
+++ ConsensusSequence.py
@@ -0,0 +1,24 @@
+"""Library containing the needed functions for assignemnt 1 for the Bioinformatics class.
+Please read: project_1_transmembrane_regions_2016.pdf for more info.
+"""
+from codes import *
+
+
+def translate(dna):
+    """Function that takes a DNA string converts it to RNA and then translates and returns its aminoacid sequence
+    Args:
+        dna (string): DNA sequence
+    Return:
+        string: Aminoacid sequence held by the DNA string given as a parameter to the function
+    """
+    rna = dna.replace('T', 'U')
+    startIndex = dna.find('AUG') + 1
+    aminoAcidsSeq = ""
+    for i in range(startIndex, len(rna), 3):
+        # codon = rna[i: i+3]
+        aminoAcidsSeq += code[rna[i: i+3]]
+        if aminoAcidsSeq[len(aminoAcidsSeq) - 1] == '*':
+            aminoAcidsSeq = aminoAcidsSeq[:-1]
+            break
+    return aminoAcidsSeq
+
---
+++ HelperFunctions.py
@@ -0,0 +1,8 @@
+def entropy(probList):
+	"""Calculate and returns the entropy based on a given list of probabilities
+	Returns:
+	    float: Return a number representing the entropy
+	Args:
+	    probList (List): List of probabilities
+	"""
+	
